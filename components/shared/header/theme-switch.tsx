30ee3dc75a49adf1c0930aaec2d2467081c8c5c1
kelechi <oparaiskelechi@gmail.com>
2025-04-22 15:20:54 +0100

    add loading and not found pages

--- components/shared/header/theme-switch.tsx
+++ components/shared/header/theme-switch.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { useTheme } from "next-themes";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
-import { SunMoon, SunIcon, MoonIcon } from "lucide-react";
+import { SunIcon, MoonIcon } from "lucide-react";
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 
