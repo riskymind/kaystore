a25f133553df59837c75527919bce8c695e7ab1a
kelechi <oparaiskelechi@gmail.com>
2025-05-16 16:22:23 +0100

    implement order page

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { insertProductSchema, cartItemSchema, insertCartSchema, shippingAddressSchema } from "@/lib/validators"
+import { insertProductSchema, cartItemSchema, insertCartSchema, shippingAddressSchema, insertReviewSchema, paymentResultSchema, insertOrderSchema, insertOrderItemSchema } from "@/lib/validators"
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string,
@@ -11,3 +11,21 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>
 export type CartItem = z.infer<typeof cartItemSchema>
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderitems: OrderItem[];
+  user: { name: string; email: string };
+  paymentResult: PaymentResult;
+};
+export type PaymentResult = z.infer<typeof paymentResultSchema>;
+export type Review = z.infer<typeof insertReviewSchema> & {
+  id: string;
+  createdAt: Date;
+  user?: { name: string };
+};
